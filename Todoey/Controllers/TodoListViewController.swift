d3f44346dee59cbd57a4ed620dff54abd37257f6
Funcraft <funcraft@hos-macbook-pro.local>
2019-01-06 16:36:42 +0800

    Converted the framework to MVC

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["chicken","Book CNY", "Truffle"]
+    var itemArray = [TodoList]()
     
     let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
+        let newitem = TodoList()
+        newitem.title = "Update Cell Fund"
+        itemArray.append(newitem)
+ 
+        let newitem2 = TodoList()
+        newitem2.title = "Get Wine for Christmas Dinner"
+        itemArray.append(newitem2)
+        
+        let newitem3 = TodoList()
+        newitem3.title = "CP eggs"
+        itemArray.append(newitem3)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [TodoList] {
+                itemArray = items
         }
+
     }
 
     //MARK - TableView Datasource Methods
@@ -36,8 +49,20 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         
-        // Configure the cell...
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternary operator
+        // value = condition ? valueiftrue : valueif false
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         
         return cell
     }
@@ -47,13 +72,9 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //tableView.deselectRow(at: <#T##IndexPath#>, animated: true)
         //itemArray[indexPath.row].completed = !itemArray[indexPath.row].completed
-        print(String(itemArray[indexPath.row]))
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
@@ -66,9 +87,11 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title:"Add New Todoey Item", message: "", preferredStyle: .alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when user clicks Add Item
-            print(textField.text)
-            self.itemArray.append(textField.text!)
-            // save in defaults
+            let newItem = TodoList()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
+            // save in defaults -- will not work here
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             print("Success!")
             self.tableView.reloadData()
---
+++ Todoey/Model/TodoList.swift
@@ -0,0 +1,21 @@
+//
+//  TodoList.swift
+//  Todoey
+//
+//  Created by Funcraft on 04/01/2019.
+//  Copyright © 2019 Funcraft. All rights reserved.
+//
+
+import Foundation
+
+class TodoList {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+//    init(todoItem: String, complete: Bool) {
+//        title = todoItem
+//        done = complete
+//    }
+}
+
